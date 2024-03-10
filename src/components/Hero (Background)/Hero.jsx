520930495f4ad1e4d749fc216800e13d6ef75e26
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-03-10 20:36:20 +0100

    Created the image for the Hero background

---
+++ src/components/Hero (Background)/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-text'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
