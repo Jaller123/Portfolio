a73570ff17388b08c22ab76647651d307caaf8e7
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-05-07 15:18:56 +0200

    Added project Page

--- src/components/Hero (Background)/Hero.jsx
+++ src/components/Hero (Background)/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
 
     <div className='hero container'>
       <div className='hero-text'>
-      <h1>Portfolio</h1>
-      <p>My name is Kenath Liyanage and I'm a FrontEnd Developer in Stockholm, Sweden. My journey in coding and design has evolved
+      <h1>Kenath</h1>
+      <p>I'm a FrontEnd Developer in Stockholm, Sweden. My journey in coding and design has evolved
         my passion to become a developer. Feel free to check out my projects and skills here!
       </p>
       <button className='btn'>Contact</button>
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import './Projects.css'
+import '../Navbar/Navbar.css'
+import Navbar from '../Navbar/Navbar'
+
+const Projects = () => 
+{
+
+  return (
+    <>
+    <div className = "bg">Projects
+    </div>
+    </>
+  )
+}
+
+export default Projects
