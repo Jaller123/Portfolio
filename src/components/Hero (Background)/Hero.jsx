83d1fb3661a163f5eb339fdf00f9346e0a2f7e88
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-05-09 14:49:51 +0200

    Added Project Slidwshow to Projects  page and instralled bootstrap

--- src/components/Hero (Background)/Hero.jsx
+++ src/components/Hero (Background)/Hero.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import './Hero.css'
-
-
+import React from 'react';
+import './Hero.css';
 
 const Hero = () => {
-  return (
-    <>
 
-    <div className='hero container'>
-      <div className='hero-text'>
-      <h1>Kenath</h1>
-      <p>I'm a FrontEnd Developer in Stockholm, Sweden. My journey in coding and design has evolved
-        my passion to become a developer. Feel free to check out my projects and skills here!
-      </p>
-      <button className='btn'>Contact</button>
+  return (
+    <div className='hero container-fluid'> 
+      <div className='row justify-content-center align-items-center'> 
+        <div className='col-md-8'> 
+          <div className='hero-text'>
+            <h1>Kenath</h1>
+            <p className="lead">I'm a FrontEnd Developer in Stockholm, Sweden. My journey in coding and design has evolved my passion to become a developer. Feel free to check out my projects and skills here!</p> {/* Added lead class for larger font */}
+            <button className='btn btn-primary'>Contact</button> 
+          </div>
+        </div>
       </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,15 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './Projects.css'
-import '../Navbar/Navbar.css'
-
+import React from 'react';
+import './Projects.css';
+import { Carousel } from 'react-bootstrap';
+import ReactPlayer from 'react-player';
+import video1 from './ProjectsFolder/alloverse.mp4'
+import video2 from './ProjectsFolder/furniturear.mp4'
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Projects = () => 
 {
-
+    const videoProperties = 
+    [
+        {
+            id: 1,
+            title: "Alloverse Amusement Park",
+            src: video1,
+            description: "This was a Ferris wheel made by me in LUA!"
+        },
+        {
+            id: 2,
+            title: "FurnitureAR",
+            src: video2,
+            description: "This is an AR App I made in C# 3 years back!"
+        }
+    ];
   return (
     <>
-    <div id="projects" className='bg'>Projects
-    </div>
+     <div id="projects" className="bg">
+            <Carousel>
+                {videoProperties.map((videoObj) => (
+                    <Carousel.Item key={videoObj.id}>
+                        <ReactPlayer url={videoObj.src} controls playing={false} /> {/* Set playing prop to false */}
+                        <Carousel.Caption>
+                            <h3>{videoObj.title}</h3>
+                            <p>{videoObj.description}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </div>
     </>
   )
 }
