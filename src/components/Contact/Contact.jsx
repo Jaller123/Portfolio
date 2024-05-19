7502a55df02cb4611aaba1ca581033559fc34bb5
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-05-19 19:10:19 +0200

    Added icons to contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Contact.css'
+import discord from './icons/discord.png'
+import linkedin from './icons/linkedin.png'
+import mail from './icons/mail.png'
+import github from './icons/github.png'
+
 
 const Contact = () => {
   return (
     <div id="contact">
-        Contact:
-        
+        <h1>Contact:</h1>
+        <div className='icons-container'>
+          <img src={discord} alt="" className='contacticon'/>
+          <img src={linkedin} alt="" className='contacticon'/>
+          <img src={mail} alt="" className='contacticon'/>
+          <img src={github} alt="" className='contacticon'/>
+          
+        </div>
     </div>
   )
 }
