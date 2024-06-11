dbb957bba0f575883cf22a6706928314d860eed6
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-06-11 12:40:22 +0200

    Reinstalled node modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero (Background)/Hero'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
-import { Navbar } from 'react-bootstrap'
+
 
 const App = () => 
 {
