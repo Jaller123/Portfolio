8006a08fc5e3ef1b57c230d092a1e86b5840e13e
Kenath Liyanage <jallerken@hotmail.se>
2024-06-11 13:26:26 +0200

    reverted everything

--- check-files.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = './src/components/Navbar/Navbar.jsx';
-
-fs.access(path, fs.constants.F_OK, (err) => {
-  console.log(`${path} ${err ? 'does not exist' : 'exists'}`);
-});
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, } from 'react'
-import Navbar from '@components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero (Background)/Hero'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
